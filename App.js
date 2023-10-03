3247c64b571f9e44ea18203f18c207db2784e75c
Lyaysan2 <lyaysan2@icloud.com>
2023-10-03 14:19:44 +0300

    hw1

--- App.js
+++ App.js
@@ -1,20 +1,118 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// const App = () => {
+//     const [pressedCount, setPressedCount] = useState(0);
+//     const [isDisabled, setDisabled] = useState(false)
+//     const handlePress = () => {
+//         setPressedCount(pressedCount+1);
+//         if (pressedCount > 2){
+//             setDisabled(true);
+//         }
+//     };
+//     const resetPress = () => {
+//         setPressedCount(0);
+//         setDisabled(false);
+//     }
+//     return (
+//         <View style={{ flex: 1, justifyContent: 'center' }}>
+//             <Text style={{ margin: 16 }}>
+//                 {pressedCount > 0
+//                     ? `The button was pressed ${pressedCount} times!`
+//                     : 'The button isn\'t pressed yet'
+//                 }
+//             </Text>
+//             <Button
+//                 title='Press me'
+//                 onPress={handlePress}
+//                 disabled={isDisabled}
+//             />
+//             <Button
+//                 title='Reset'
+//                 onPress={resetPress}
+//             />
+//         </View>
+//     );
+// }
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+// const App = () => {
+//     const [name, setName] = useState('');
+//     const [text, setText] = useState('');
+//     const handlePress = () => {
+//         setText('');
+//         setName(text);
+//     }
+//     return (
+//         <View style={{
+//             flex: 1,
+//             alignContent: 'center',
+//             justifyContent: 'center',
+//             padding: 16,
+//         }}>
+//             <Text style={{ marginVertical: 5 }}>
+//                 {name ? `Hi ${name}!` : 'What is your name?'}
+//             </Text>
+//             <Text style={{ marginVertical: 5 }}>
+//                 {text}
+//             </Text>
+//             <TextInput
+//                 style={{ padding: 8, backgroundColor: '#f5f5f5' }}
+//                 onChangeText={t => setText(t)}
+//             />
+//             <Button
+//                 title='Button'
+//                 onPress={handlePress}
+//             />
+//         </View>
+//     );
+// };
+
+// const App = () => (
+//     <View style={{ flex: 1, justifyContent: 'center' }}>
+//         <Box width={50} height={60} color='red'></Box>
+//         <Box width={70} height={80} color='green'></Box>
+//         <Box width={90} height={100} color='blue'></Box>
+//     </View>
+// );
+//
+// export const Box = (props) => (
+//     <View style={{ width: props.width, height: props.height, backgroundColor:
+//         props.color}} />
+// );
+
+import React from 'react';
+import Hw1 from "./tasks/Hw1";
+import Hw2 from "./tasks/Hw2";
+import Hw3 from "./tasks/Hw3";
+import {View, Text, Button} from 'react-native';
+import {NavigationContainer} from "@react-navigation/native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen({navigation}) {
+    return (
+        <View>
+            <Text>Main Page</Text>
+            <Button onPress={() => navigation.navigate('Hw1')}
+                    title="Hw1"/>
+            <Button onPress={() => navigation.navigate('Hw2')}
+                    title="Hw2"/>
+            <Button onPress={() => navigation.navigate('Hw3')}
+                    title="Hw3"/>
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name={'Home'} component={HomeScreen}/>
+                <Stack.Screen name={'Hw1'} component={Hw1}/>
+                <Stack.Screen name={'Hw2'} component={Hw2}/>
+                <Stack.Screen name={'Hw3'} component={Hw3}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default App;
